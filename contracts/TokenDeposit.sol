6189c78894e5c3effbe96aed6e7a23d4ef3b3d24
kacharlaswathi <kacharlaswathi451@gmail.com>
2022-12-08 12:50:16 +0530

    add slab address to array

--- contracts/TokenDeposit.sol
+++ contracts/TokenDeposit.sol
@@ -6,19 +6,19 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract TokenDeposit {
 
-    address[] public slabs;
+    address[5] public slabs;
     mapping(uint256 => uint256) public slabsOccupied;
 
     constructor() {
-        slabs.push(new Slab(100));
-        slabs.push(new Slab(200));
-        slabs.push(new Slab(300));
-        slabs.push(new Slab(400));
-        slabs.push(new Slab(500));
+        slabs[0] = address(new Slab(100));
+        slabs[1] = address(new Slab(200));
+        slabs[2] = address(new Slab(300));
+        slabs[3] = address(new Slab(400));
+        slabs[4] = address(new Slab(500));
     }
 
     function deposit(address _token, uint256 _amount) external {
-        IERC20(_token).transferFrom(msg.sender, slabs[slabs.length - 1]);
+        IERC20(_token).transferFrom(msg.sender, slabs[slabs.length - 1], _amount);
 
     }
 }
